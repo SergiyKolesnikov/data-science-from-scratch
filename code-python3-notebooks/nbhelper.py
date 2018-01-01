4096346734007f99f6060278e931fc9982f7565f
Sergiy <SergiyKolesnikov@users.noreply.github.com>
2018-01-01 22:56:36 +0100

    Add nbhelper module.

---
+++ code-python3-notebooks/nbhelper.py
@@ -0,0 +1,13 @@
+'''
+Helper functions for Jupyter notebooks. This code is not from the book.
+'''
+import sys, os
+import matplotlib.pyplot as plt
+
+# This is Needed for importing our linear algebra and other functions.
+sys.path.append(os.path.join('..','code-python3'))
+
+def double_x_size():
+    '''Return the size of the current figure with the doubled X size.
+       Used to set the size of the figure for two plots.'''
+    return plt.gcf().get_size_inches() * (2, 1)
