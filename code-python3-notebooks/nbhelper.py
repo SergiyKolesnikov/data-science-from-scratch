1663477355017e5f6ac8c79b6bd0fda68cb95600
Sergiy Kolesnikov <SergiyKolesnikov@users.noreply.github.com>
2018-01-07 18:58:53 +0100

    Add plot-size calculation functions.

--- code-python3-notebooks/nbhelper.py
+++ code-python3-notebooks/nbhelper.py
@@ -11,3 +11,13 @@ def double_x_size():
     '''Return the size of the current figure with the doubled X size.
        Used to set the size of the figure for two plots.'''
     return plt.gcf().get_size_inches() * (2, 1)
+
+def double_y_size():
+    '''Return the size of the current figure with the doubled Y size.
+       Used to set the size of the figure for two plots.'''
+    return plt.gcf().get_size_inches() * (1, 2)
+
+def double_xy_size():
+    '''Return the size of the current figure with the doubled X and Y size.
+       Used to set the size of the figure for two plots.'''
+    return plt.gcf().get_size_inches() * (2, 2)
